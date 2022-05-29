a73a3baa499aa16306cb23d389e7559354908a46
bhargavshivashankar <bhargavshivashankar12@gmail.com>
2022-05-29 20:00:16 +0530

    Added takecommand to jarvis file[C

--- jarvis.py
+++ jarvis.py
@@ -1,5 +1,8 @@
+from email.mime import audio
 import pyttsx3
 import datetime
+import speech_recognition as sr
+
 
 
 engine = pyttsx3.init('sapi5')
@@ -25,6 +28,27 @@ def wishme():
         speak("Good Evening!")
     speak("I am Jarvis. Please tell me how may i help you")
 
+def takecommand():
+    r = sr.Recognizer()
+    with sr.Microphone() as source:
+        print("Listening...")
+        r.pause_threshold = 1
+        audio = r.listen(source)
+
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+        print(f"User said: {query}\n")
+
+    except Exception as e:
+        print("Please say again...")
+        return "None"
+    return query
+
+
 if __name__ =="__main__":
     speak("Hello Bhargav.")
     wishme()
+    
+    while True:
+        query = takecommand().lower()
